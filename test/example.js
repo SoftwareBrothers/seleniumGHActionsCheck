2b21df3dca7705bc9d65b05928efe63f4d49a352
wojciechnowacki <wojciech.nowacki@softwarebrothers.co>
2020-01-31 08:54:08 +0100

    one test and extra wait

--- test/example.js
+++ test/example.js
@@ -13,23 +13,10 @@ describe('DefaultTest', () => {
     before(() => {
         driver = new Builder().forBrowser('chrome').build()
     })
+    await driver.sleep(10000);
 
     it('should go to nehalist.io and check the title', async () => {
-        await driver.sleep(10000);
 
-        await driver.manage().window().maximize();
-        await driver.get('https://softwarebrothers.co/');
-
-        await driver.findElement(By.linkText("Services")).click();
-        await driver.sleep(2000);
-        await driver.findElement(By.className(("webdev"))).click();
-        await driver.sleep(2000);
-        const title = await (await driver.findElement(By.xpath("//*[text()='Web Design and Development']"))).getText();
-        expect(title.toUpperCase()).to.have.string('WEB DESIGN AND DEVELOPMENT');
-        await driver.sleep(2000);
-
-    });
-    it('should go to nehalist.io and check the title', async () => {
         await driver.manage().window().maximize();
         await driver.get('https://softwarebrothers.co/');
         await driver.sleep(10000);
@@ -43,20 +30,7 @@ describe('DefaultTest', () => {
         await driver.sleep(2000);
 
     });
-    it('should go to nehalist.io and check the title', async () => {
-        await driver.manage().window().maximize();
-        await driver.get('https://softwarebrothers.co/');
-        await driver.sleep(10000);
 
-        await driver.findElement(By.linkText("Services")).click();
-        await driver.sleep(2000);
-        await driver.findElement(By.className(("webdev"))).click();
-        await driver.sleep(2000);
-        const title = await (await driver.findElement(By.xpath("//*[text()='Web Design and Development']"))).getText();
-        expect(title.toUpperCase()).to.have.string('WEB DESIGN AND DEVELOPMENT');
-        await driver.sleep(2000);
-
-    });
     after(async () => {
         driver.close()
         driver.quit()
