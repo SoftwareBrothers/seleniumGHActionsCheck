16163bf084185a825d67f8f37e885e303d2ef586
wojciechnowacki <wojciech.nowacki@softwarebrothers.co>
2020-02-03 12:50:58 +0100

    moj komentarz2

--- test/example.js
+++ test/example.js
@@ -1,83 +1,83 @@
-const {
-    Builder,
-    By,
-    Key,
-    until,
-    WebDriver,
-    Capabilities
-} = require('selenium-webdriver');
-const {
-    expect
-} = require('chai');
-const {
-    Eyes,
-    ClassicRunner,
-    Target,
-    RectangleSize
-} = require('@applitools/eyes-selenium');
+// const {
+//     Builder,
+//     By,
+//     Key,
+//     until,
+//     WebDriver,
+//     Capabilities
+// } = require('selenium-webdriver');
+// const {
+//     expect
+// } = require('chai');
+// const {
+//     Eyes,
+//     ClassicRunner,
+//     Target,
+//     RectangleSize
+// } = require('@applitools/eyes-selenium');
 
-describe('DefaultTest', () => {
+// describe('DefaultTest', () => {
 
-    let driver;
-    let eyes;
+//     let driver;
+//     let eyes;
 
-    beforeEach(async () => {
-        //let capabilities = webdriver.Capabilities.safari();
-        driver = await new Builder().forBrowser('chrome').build();
-        //   runner = new ClassicRunner();
-        //  eyes = new Eyes(runner);
-        //  eyes.setApiKey('L7FtaWHGMkDYVj111K6pD101qPr0RaFb8sYADTANemhrHdQ110');
-    })
+//     beforeEach(async () => {
+//         //let capabilities = webdriver.Capabilities.safari();
+//         driver = await new Builder().forBrowser('chrome').build();
+//         //   runner = new ClassicRunner();
+//         //  eyes = new Eyes(runner);
+//         //  eyes.setApiKey('L7FtaWHGMkDYVj111K6pD101qPr0RaFb8sYADTANemhrHdQ110');
+//     })
 
-    it('Just testing flow for selenium in SB company', async () => {
-        //    await eyes.open(driver, 'Demo', 'Sample test');
-        await driver.manage().window().maximize();
-        await driver.get('https://softwarebrothers.co/');
-        await driver.findElement(By.linkText("Services")).click();
-        await driver.sleep(2000);
-        //  await eyes.check("Login Window", Target.window())
-        await driver.wait(until.elementIsVisible(driver.findElement(By.xpath("//a[@aria-label= 'dismiss cookie message' and @role='button']")))).click();
-        await driver.sleep(2000);
-        await driver.findElement(By.className(("webdev"))).click();
-        //    await eyes.check("Login Window", Target.window())
-        await driver.sleep(2000);
-        const title = await (await driver.findElement(By.xpath("//*[text()='Web Design and Development']"))).getText();
-        expect(title.toUpperCase()).to.have.string('WEB DESIGN AND DEVELOPMENT');
-    });
-    // it('should go to nehalist.io and check the title', async () => {
-    //     await driver.manage().window().maximize();
-    //     await driver.get('https://softwarebrothers.co/');
-    //     await driver.findElement(By.linkText("Services")).click();
-    //     await driver.sleep(2000);
-    //     await driver.wait(until.elementIsVisible(driver.findElement(By.xpath("//a[@aria-label= 'dismiss cookie message' and @role='button']")))).click();
-    //     await driver.sleep(2000);
-    //     await driver.findElement(By.className(("webdev"))).click();
-    //     await driver.sleep(2000);
-    //     const title = await (await driver.findElement(By.xpath("//*[text()='Web Design and Development']"))).getText();
-    //     expect(title.toUpperCase()).to.have.string('WEB DESIGN AND DEVELOPMENT');
-    //     await driver.sleep(2000);
-    // });
+//     it('Just testing flow for selenium in SB company', async () => {
+//         //    await eyes.open(driver, 'Demo', 'Sample test');
+//         await driver.manage().window().maximize();
+//         await driver.get('https://softwarebrothers.co/');
+//         await driver.findElement(By.linkText("Services")).click();
+//         await driver.sleep(2000);
+//         //  await eyes.check("Login Window", Target.window())
+//         await driver.wait(until.elementIsVisible(driver.findElement(By.xpath("//a[@aria-label= 'dismiss cookie message' and @role='button']")))).click();
+//         await driver.sleep(2000);
+//         await driver.findElement(By.className(("webdev"))).click();
+//         //    await eyes.check("Login Window", Target.window())
+//         await driver.sleep(2000);
+//         const title = await (await driver.findElement(By.xpath("//*[text()='Web Design and Development']"))).getText();
+//         expect(title.toUpperCase()).to.have.string('WEB DESIGN AND DEVELOPMENT');
+//     });
+//     // it('should go to nehalist.io and check the title', async () => {
+//     //     await driver.manage().window().maximize();
+//     //     await driver.get('https://softwarebrothers.co/');
+//     //     await driver.findElement(By.linkText("Services")).click();
+//     //     await driver.sleep(2000);
+//     //     await driver.wait(until.elementIsVisible(driver.findElement(By.xpath("//a[@aria-label= 'dismiss cookie message' and @role='button']")))).click();
+//     //     await driver.sleep(2000);
+//     //     await driver.findElement(By.className(("webdev"))).click();
+//     //     await driver.sleep(2000);
+//     //     const title = await (await driver.findElement(By.xpath("//*[text()='Web Design and Development']"))).getText();
+//     //     expect(title.toUpperCase()).to.have.string('WEB DESIGN AND DEVELOPMENT');
+//     //     await driver.sleep(2000);
+//     // });
 
-    // it('should go to nehalist.io and check the title', async () => {
-    //     await driver.manage().window().maximize();
-    //     await driver.get('https://softwarebrothers.co/');
-    //     await driver.findElement(By.linkText("Services")).click();
-    //     await driver.sleep(2000);
-    //     await driver.wait(until.elementIsVisible(driver.findElement(By.xpath("//a[@aria-label= 'dismiss cookie message' and @role='button']")))).click();
-    //     await driver.sleep(2000);
-    //     await driver.findElement(By.className(("webdev"))).click();
-    //     await driver.sleep(2000);
-    //     const title = await (await driver.findElement(By.xpath("//*[text()='Web Design and Development']"))).getText();
-    //     expect(title.toUpperCase()).to.have.string('WEB DESIGN AND DEVELOPMENT');
-    //     await driver.sleep(2000);
-    // });
+//     // it('should go to nehalist.io and check the title', async () => {
+//     //     await driver.manage().window().maximize();
+//     //     await driver.get('https://softwarebrothers.co/');
+//     //     await driver.findElement(By.linkText("Services")).click();
+//     //     await driver.sleep(2000);
+//     //     await driver.wait(until.elementIsVisible(driver.findElement(By.xpath("//a[@aria-label= 'dismiss cookie message' and @role='button']")))).click();
+//     //     await driver.sleep(2000);
+//     //     await driver.findElement(By.className(("webdev"))).click();
+//     //     await driver.sleep(2000);
+//     //     const title = await (await driver.findElement(By.xpath("//*[text()='Web Design and Development']"))).getText();
+//     //     expect(title.toUpperCase()).to.have.string('WEB DESIGN AND DEVELOPMENT');
+//     //     await driver.sleep(2000);
+//     // });
 
-    afterEach(async () => {
-        //     await eyes.closeAsync();
-        await driver.quit();
-        //   await eyes.abortIfNotClosed();
-        // const allTestResults = await runner.getAllTestResults();
-        //  console.log(allTestResults);
-    });
+//     afterEach(async () => {
+//         //     await eyes.closeAsync();
+//         await driver.quit();
+//         //   await eyes.abortIfNotClosed();
+//         // const allTestResults = await runner.getAllTestResults();
+//         //  console.log(allTestResults);
+//     });
 
-});
+// });
--- test/randome.js
+++ test/randome.js
@@ -5,54 +5,56 @@ const path = require("path");
 
 let driver = webdriver.WebDriver;
 
-beforeAll(async () => {
-    let capabilities = webdriver.Capabilities;
-    switch (process.env.BROWSER || "chrome") {
-        case "ie": {
-            // HACK: include IEDriver path by nuget
-            const driverPath = path.join(
-                __dirname,
-                "../Selenium.WebDriver.IEDriver.3.150.0/driver/"
-            );
-            process.env.PATH = `${process.env.PATH};${driverPath};`;
-            capabilities = webdriver.Capabilities.ie();
-            capabilities.set("ignoreProtectedModeSettings", true);
-            capabilities.set("ignoreZoomSetting", true);
-            break;
+describe('my test', () => {
+    before(async () => {
+        let capabilities = webdriver.Capabilities;
+        switch (process.env.BROWSER || "chrome") {
+            case "ie": {
+                // HACK: include IEDriver path by nuget
+                const driverPath = path.join(
+                    __dirname,
+                    "../Selenium.WebDriver.IEDriver.3.150.0/driver/"
+                );
+                process.env.PATH = `${process.env.PATH};${driverPath};`;
+                capabilities = webdriver.Capabilities.ie();
+                capabilities.set("ignoreProtectedModeSettings", true);
+                capabilities.set("ignoreZoomSetting", true);
+                break;
+            }
+            case "safari": {
+                capabilities = webdriver.Capabilities.safari();
+                break;
+            }
+            case "firefox": {
+                require("geckodriver");
+                capabilities = webdriver.Capabilities.firefox();
+                break;
+            }
+            case "chrome": {
+                require("chromedriver");
+                capabilities = webdriver.Capabilities.chrome();
+                capabilities.set("chromeOptions", {
+                    args: [
+                        "--headless",
+                        "--no-sandbox",
+                        "--disable-gpu",
+                        "--window-size=1980,1200"
+                    ]
+                });
+                break;
+            }
         }
-        case "safari": {
-            capabilities = webdriver.Capabilities.safari();
-            break;
-        }
-        case "firefox": {
-            require("geckodriver");
-            capabilities = webdriver.Capabilities.firefox();
-            break;
-        }
-        case "chrome": {
-            require("chromedriver");
-            capabilities = webdriver.Capabilities.chrome();
-            capabilities.set("chromeOptions", {
-                args: [
-                    "--headless",
-                    "--no-sandbox",
-                    "--disable-gpu",
-                    "--window-size=1980,1200"
-                ]
-            });
-            break;
-        }
-    }
-    driver = await new webdriver.Builder()
-        .withCapabilities(capabilities)
-        .build();
-});
+        driver = await new webdriver.Builder()
+            .withCapabilities(capabilities)
+            .build();
+    });
 
-afterAll(async () => {
-    await driver.quit()
-});
+    after(async () => {
+        await driver.quit()
+    });
 
-it("Google", async () => {
-    await driver.get('http://google.com');
-    assert.equal(await driver.getTitle(), "Google");
+    it("Google", async () => {
+        await driver.get('http://google.com');
+        assert.equal(await driver.getTitle(), "Google");
+    });
 });
